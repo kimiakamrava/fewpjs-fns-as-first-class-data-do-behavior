fbb6df7618f80d89564a10ab1c86fe3c181bb6e7
kimiakamrava <kimiakamrava93@gmail.com>
2021-01-13 16:38:53 -0800

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  let hour = parseInt(time.split(0, 2))
+  if (hour < 12) {
+    return "Good Morning"
+  }
+  else if (hour < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
+/* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  let node = document.querySelector("#greeting");
+  node.innerText = msg;
+}
